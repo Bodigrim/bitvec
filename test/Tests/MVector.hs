3a0e14868cf4560d523d52915998d3f22b562b36
Bodigrim <andrew.lelechenko@gmail.com>
2019-01-13 02:10:10 +0000

    Test MVector rules

--- test/Tests/MVector.hs
+++ test/Tests/MVector.hs
@@ -5,15 +5,20 @@ import Support
 import Control.Monad
 import Control.Monad.ST
 import Data.Bit
+import Data.Proxy
 import Data.STRef
 import qualified Data.Vector.Generic             as V
+import qualified Data.Vector.Generic.Mutable     as M (basicInitialize, basicSet)
 import qualified Data.Vector.Generic.New         as N
 import qualified Data.Vector.Unboxed.Bit         as B
 import qualified Data.Vector.Unboxed.Mutable.Bit as U
 import qualified Data.Vector.Unboxed.Mutable     as M
 import Test.Framework (Test, testGroup)
+import Test.Framework.Providers.HUnit (testCase)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.HUnit (assertEqual)
 import Test.QuickCheck
+import Test.QuickCheck.Classes
 
 mvectorTests :: Test
 mvectorTests = testGroup "Data.Vector.Unboxed.Mutable.Bit"
@@ -35,7 +40,142 @@ mvectorTests = testGroup "Data.Vector.Unboxed.Mutable.Bit"
     , testProperty "countBits"      prop_countBits_def
     , testProperty "listBits"       prop_listBits_def
     , testProperty "reverseInPlace" prop_reverseInPlace_def
+    , testGroup "MVector laws" $ map (uncurry testProperty) $ lawsProperties $ muvectorLaws (Proxy :: Proxy Bit)
+    , testCase "basicInitialize 1" case_write_init_read1
+    , testCase "basicInitialize 2" case_write_init_read2
+    , testCase "basicInitialize 3" case_write_init_read3
+    , testCase "basicInitialize 4" case_write_init_read4
+    , testCase "basicSet 1" case_write_set_read1
+    , testCase "basicSet 2" case_write_set_read2
+    , testCase "basicSet 3" case_write_set_read3
+    , testCase "basicSet 4" case_write_set_read4
+    , testCase "basicSet 5" case_set_read1
+    , testCase "basicSet 6" case_set_read2
+    , testCase "basicSet 7" case_set_read3
+    , testCase "basicUnsafeCopy1" case_write_copy_read1
+    , testCase "basicUnsafeCopy2" case_write_copy_read2
+    , testCase "basicUnsafeCopy3" case_write_copy_read3
+    , testCase "basicUnsafeCopy4" case_write_copy_read4
+    , testCase "basicUnsafeCopy5" case_write_copy_read5
     ]
+
+case_write_init_read1 :: IO ()
+case_write_init_read1 = assertEqual "should be equal" (fromBool True) $ runST $ do
+    arr <- M.new 2
+    M.write arr 0 (fromBool True)
+    M.basicInitialize (M.slice 1 1 arr)
+    M.read arr 0
+
+case_write_init_read2 :: IO ()
+case_write_init_read2 = assertEqual "should be equal" (fromBool True) $ runST $ do
+    arr <- M.new 2
+    M.write arr 1 (fromBool True)
+    M.basicInitialize (M.slice 0 1 arr)
+    M.read arr 1
+
+case_write_init_read3 :: IO ()
+case_write_init_read3 = assertEqual "should be equal" (fromBool True, fromBool True) $ runST $ do
+    arr <- M.new 2
+    M.write arr 0 (fromBool True)
+    M.write arr 1 (fromBool True)
+    M.basicInitialize (M.slice 1 0 arr)
+    (,) <$> M.read arr 0 <*> M.read arr 1
+
+case_write_init_read4 :: IO ()
+case_write_init_read4 = assertEqual "should be equal" (fromBool True, fromBool True) $ runST $ do
+    arr <- M.new 3
+    M.write arr 0 (fromBool True)
+    M.write arr 2 (fromBool True)
+    M.basicInitialize (M.slice 1 1 arr)
+    (,) <$> M.read arr 0 <*> M.read arr 2
+
+case_write_set_read1 :: IO ()
+case_write_set_read1 = assertEqual "should be equal" (fromBool True) $ runST $ do
+    arr <- M.new 2
+    M.write arr 0 (fromBool True)
+    M.basicSet (M.slice 1 1 arr) (fromBool False)
+    M.read arr 0
+
+case_write_set_read2 :: IO ()
+case_write_set_read2 = assertEqual "should be equal" (fromBool True) $ runST $ do
+    arr <- M.new 2
+    M.write arr 1 (fromBool True)
+    M.basicSet (M.slice 0 1 arr) (fromBool False)
+    M.read arr 1
+
+case_write_set_read3 :: IO ()
+case_write_set_read3 = assertEqual "should be equal" (fromBool True, fromBool True) $ runST $ do
+    arr <- M.new 2
+    M.write arr 0 (fromBool True)
+    M.write arr 1 (fromBool True)
+    M.basicSet (M.slice 1 0 arr) (fromBool False)
+    (,) <$> M.read arr 0 <*> M.read arr 1
+
+case_write_set_read4 :: IO ()
+case_write_set_read4 = assertEqual "should be equal" (fromBool True, fromBool True) $ runST $ do
+    arr <- M.new 3
+    M.write arr 0 (fromBool True)
+    M.write arr 2 (fromBool True)
+    M.basicSet (M.slice 1 1 arr) (fromBool False)
+    (,) <$> M.read arr 0 <*> M.read arr 2
+
+case_set_read1 :: IO ()
+case_set_read1 = assertEqual "should be equal" (fromBool True) $ runST $ do
+    arr <- M.new 1
+    M.basicSet arr (fromBool True)
+    M.read arr 0
+
+case_set_read2 :: IO ()
+case_set_read2 = assertEqual "should be equal" (fromBool True) $ runST $ do
+    arr <- M.new 2
+    M.basicSet (M.slice 1 1 arr) (fromBool True)
+    M.read arr 1
+
+case_set_read3 :: IO ()
+case_set_read3 = assertEqual "should be equal" (fromBool True) $ runST $ do
+    arr <- M.new 192
+    M.basicSet (M.slice 71 121 arr) (fromBool True)
+    M.read arr 145
+
+case_write_copy_read1 :: IO ()
+case_write_copy_read1 = assertEqual "should be equal" (fromBool True) $ runST $ do
+    src <- M.slice 37 28 <$> M.new 65
+    M.write src 27 (fromBool True)
+    dst <- M.slice 37 28 <$> M.new 65
+    M.copy dst src
+    M.read dst 27
+
+case_write_copy_read2 :: IO ()
+case_write_copy_read2 = assertEqual "should be equal" (fromBool True) $ runST $ do
+    src <- M.slice 32 33 <$> M.new 65
+    M.write src 0 (fromBool True)
+    dst <- M.slice 32 33 <$> M.new 65
+    M.copy dst src
+    M.read dst 0
+
+case_write_copy_read3 :: IO ()
+case_write_copy_read3 = assertEqual "should be equal" (fromBool True) $ runST $ do
+    src <- M.slice 1 1 <$> M.new 2
+    M.write src 0 (fromBool True)
+    dst <- M.slice 1 1 <$> M.new 2
+    M.copy dst src
+    M.read dst 0
+
+case_write_copy_read4 :: IO ()
+case_write_copy_read4 = assertEqual "should be equal" (fromBool True) $ runST $ do
+    src <- M.slice 12 52 <$> M.new 64
+    M.write src 22 (fromBool True)
+    dst <- M.slice 12 52 <$> M.new 64
+    M.copy dst src
+    M.read dst 22
+
+case_write_copy_read5 :: IO ()
+case_write_copy_read5 = assertEqual "should be equal" (fromBool True) $ runST $ do
+    src <- M.slice 48 80 <$> M.new 128
+    M.write src 46 (fromBool True)
+    dst <- M.slice 48 80 <$> M.new 128
+    M.copy dst src
+    M.read dst 46
 
 prop_slice_def :: Int -> Int -> N.New U.Vector Bit -> Bool
 prop_slice_def s n xs = runST $ do
