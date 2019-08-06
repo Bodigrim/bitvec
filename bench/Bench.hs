80df3aa25b1ca53d170f01f4aa69c1ee08c9ea28
Bodigrim <andrew.lelechenko@gmail.com>
2019-08-06 21:07:20 +0100

    Benchmark invertInPlace

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Gauge.Main
 
+import Bench.Invert
 import Bench.RandomFlip
 import Bench.RandomRead
 import Bench.RandomWrite
@@ -9,7 +10,8 @@ import Bench.Reverse
 
 main :: IO ()
 main = defaultMain
-  [ bgroup "randomWrite" $ map benchRandomWrite   [5..10]
+  [ bgroup "invert"      $ map benchInvert        [5..10]
+  , bgroup "randomWrite" $ map benchRandomWrite   [5..10]
   , bgroup "randomFlip"  $ map benchRandomFlip    [5..10]
   , bgroup "randomRead"  $ map benchRandomRead    [5..10]
   , bgroup "reverse"     $ map benchReverse       [5..10]
---
+++ bench/Bench/Invert.hs
@@ -0,0 +1,39 @@
+module Bench.Invert
+  ( benchInvert
+  ) where
+
+import Data.Bit
+import qualified Data.Bit.ThreadSafe as TS
+import Data.Bits
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as MU
+import Gauge.Main
+import System.Random
+
+randomBools :: [Bool]
+randomBools
+  = map (\i -> if i > (0 :: Int) then True else False)
+  . randoms
+  . mkStdGen
+  $ 42
+
+randomVec :: MU.Unbox a => (Bool -> a) -> Int -> U.Vector a
+randomVec f k = U.fromList (map f (take n randomBools))
+  where
+    n = 1 `shiftL` k
+
+benchInvert :: Int -> Benchmark
+benchInvert k = bgroup (show (1 `shiftL` k :: Int))
+  [ bench "Bit"    $ nf invertBit    (randomVec Bit k)
+  , bench "Bit.TS" $ nf invertBitTS  (randomVec TS.Bit k)
+  , bench "Vector" $ nf invertVector (randomVec id k)
+  ]
+
+invertBit :: U.Vector Bit -> U.Vector Bit
+invertBit = U.modify invertInPlace
+
+invertBitTS :: U.Vector TS.Bit -> U.Vector TS.Bit
+invertBitTS = U.modify TS.invertInPlace
+
+invertVector :: U.Vector Bool -> U.Vector Bool
+invertVector = U.map not
