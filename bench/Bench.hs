a6f225e758f4dcd4b9bdfc6c6d8e70d4dd26cbe3
Bodigrim <andrew.lelechenko@gmail.com>
2019-08-22 21:57:14 +0100

    #18 Benchmark multiplication of F2Poly

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -5,6 +5,7 @@ import Gauge.Main
 import Bench.BitIndex
 import Bench.Intersection
 import Bench.Invert
+import Bench.Product
 import Bench.RandomFlip
 import Bench.RandomRead
 import Bench.RandomWrite
@@ -13,12 +14,13 @@ import Bench.Union
 
 main :: IO ()
 main = defaultMain
-  [ bgroup "bitIndex"     $ map benchBitIndex      [5..10]
-  , bgroup "invert"       $ map benchInvert        [5..10]
-  , bgroup "intersection" $ map benchIntersection  [5..10]
-  , bgroup "randomWrite"  $ map benchRandomWrite   [5..10]
-  , bgroup "randomFlip"   $ map benchRandomFlip    [5..10]
-  , bgroup "randomRead"   $ map benchRandomRead    [5..10]
-  , bgroup "reverse"      $ map benchReverse       [5..10]
-  , bgroup "union"        $ map benchUnion         [5..10]
+  [ bgroup "bitIndex"     $ map benchBitIndex     [5..10]
+  , bgroup "invert"       $ map benchInvert       [5..10]
+  , bgroup "intersection" $ map benchIntersection [5..10]
+  , bgroup "product"      $ map benchProduct      [5..10]
+  , bgroup "randomWrite"  $ map benchRandomWrite  [5..10]
+  , bgroup "randomFlip"   $ map benchRandomFlip   [5..10]
+  , bgroup "randomRead"   $ map benchRandomRead   [5..10]
+  , bgroup "reverse"      $ map benchReverse      [5..10]
+  , bgroup "union"        $ map benchUnion        [5..10]
   ]
---
+++ bench/Bench/Product.hs
@@ -0,0 +1,40 @@
+module Bench.Product
+  ( benchProduct
+  ) where
+
+import Data.Bit
+import qualified Data.Bit.ThreadSafe as TS
+import Data.Bits
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as MU
+import Gauge.Main
+import System.Random
+
+randomBools :: [Bool]
+randomBools
+  = map (\i -> if i > (0 :: Int) then True else False)
+  . randoms
+  . mkStdGen
+  $ 42
+
+randomVec :: MU.Unbox a => (Bool -> a) -> Int -> U.Vector a
+randomVec f k = U.fromList (map f (take n randomBools))
+  where
+    n = 1 `shiftL` k
+
+randomVec2 :: MU.Unbox a => (Bool -> a) -> Int -> U.Vector a
+randomVec2 f k = U.fromList (map f (take n $ drop n randomBools))
+  where
+    n = 1 `shiftL` k
+
+benchProduct :: Int -> Benchmark
+benchProduct k = bgroup (show (1 `shiftL` k :: Int))
+  [ bench "Bit/product"    $ nf (\x -> productBit    (randomVec Bit k) x)    (randomVec2 Bit k)
+  , bench "Bit.TS/product" $ nf (\x -> productBitTS  (randomVec TS.Bit k) x) (randomVec2 TS.Bit k)
+  ]
+
+productBit :: U.Vector Bit -> U.Vector Bit -> U.Vector Bit
+productBit xs ys = unF2Poly (F2Poly xs * F2Poly ys)
+
+productBitTS :: U.Vector TS.Bit -> U.Vector TS.Bit -> U.Vector TS.Bit
+productBitTS xs ys = TS.unF2Poly (TS.F2Poly xs * TS.F2Poly ys)
