953e9fe4e75bf42dad3b4b848088fd96848770e9
Bodigrim <andrew.lelechenko@gmail.com>
2019-08-27 21:02:05 +0100

    Revert "#20 Replace forall a. Bits a with forall a. FiniteBits a"
    This reverts commit d90cf44fcd3fcf7bd1df60d7954b7a5c1a3b3590.
    
    # Conflicts:
    #	src/Data/Bit/Immutable.hs

--- src/Data/Bit/Immutable.hs
+++ src/Data/Bit/Immutable.hs
@@ -91,7 +91,7 @@ cloneToWords v = runST $ do
 -- >>> zipBits xor (read "[1,1,0]") (read "[0,1,1]") -- symmetric difference
 -- [1,0,1]
 zipBits
-  :: (forall a . FiniteBits a => a -> a -> a)
+  :: (forall a . Bits a => a -> a -> a)
   -> U.Vector Bit
   -> U.Vector Bit
   -> U.Vector Bit
