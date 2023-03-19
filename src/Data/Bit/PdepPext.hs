df364e723ff71329906e42776a9d9c86072df47d
Bodigrim <andrew.lelechenko@gmail.com>
2023-03-19 23:03:40 +0000

    Drop support of GHC < 8.4

--- src/Data/Bit/PdepPext.hs
+++ src/Data/Bit/PdepPext.hs
@@ -6,15 +6,12 @@
 -- | Parallel bit deposit and extract instructions.
 -- https://en.wikipedia.org/wiki/Bit_Manipulation_Instruction_Sets#Parallel_bit_deposit_and_extract
 
-{-# LANGUAGE CPP          #-}
 {-# LANGUAGE MagicHash    #-}
 
 module Data.Bit.PdepPext
   ( pdep
   , pext
   ) where
-
-#if MIN_VERSION_base(4,11,0)
 
 import GHC.Exts
 
@@ -23,32 +20,3 @@ pdep (W# src#) (W# mask#) = W# (pdep# src# mask#)
 
 pext :: Word -> Word -> Word
 pext (W# src#) (W# mask#) = W# (pext# src# mask#)
-
-#else
-
-import Data.Bits
-
-pdep :: Word -> Word -> Word
-pdep = go 0
-  where
-    go :: Word -> Word -> Word -> Word
-    go result _ 0 = result
-    go result src mask = go newResult newSrc newMask
-      where
-        lowest    = 1 `shiftL` countTrailingZeros mask
-        newResult = if src .&. 1 == 0 then result else result .|. lowest
-        newSrc    = src `shiftR` 1
-        newMask   = mask .&. complement lowest
-
-pext :: Word -> Word -> Word
-pext src mask = loop 0 0 0
-  where
-    loop i count acc
-      | i >= finiteBitSize (0 :: Word)
-      = acc
-      | testBit mask i
-      = loop (i + 1) (count + 1) (if testBit src i then setBit acc count else acc)
-      | otherwise
-      = loop (i + 1) count acc
-
-#endif
